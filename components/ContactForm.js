db69120305ffa512c99a64c04857b5d6636ffec8
Ewa-K <ewa-1988@yahoo.com>
2018-06-14 14:42:05 +0200

    Change project

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,9 +6,9 @@ var ContactForm = React.createClass({
   render: function() {
     return (
       <form className={'contactForm container form-inline'}>
-        <input className={'form-control'} type={'text'} placeholder={'Imię'} value={'this.props.contact.firstName'}>
-        <input className={'form-control'} type={'text'} placeholder={'Nazwisko'} value={'this.props.contact.lastName'}>
-        <input className={'form-control'} type={'email'} placeholder={'Email'} value={'this.props.contact.email'}>
+        <input className={'form-control'} type={'text'} placeholder={'Imię'} value={'this.props.contact.firstName'}/>
+        <input className={'form-control'} type={'text'} placeholder={'Nazwisko'} value={'this.props.contact.lastName'}/>
+        <input className={'form-control'} type={'email'} placeholder={'Email'} value={'this.props.contact.email'}/>
         <button type={'submit'} className={'btn btn-primary'}>
           Dodaj kontakt
         </button>
